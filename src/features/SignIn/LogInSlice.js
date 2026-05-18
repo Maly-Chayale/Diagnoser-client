6ee48328da8e73b89b047acb2a955e248c98fada
HenyaKatz <Hc216307900@sgur.org.il>
2026-05-18 14:32:48 +0300

    add lead

--- src/features/SignIn/LogInSlice.js
+++ src/features/SignIn/LogInSlice.js
@@ -16,8 +16,9 @@ export const addLead = createAsyncThunk("addLead",
         await axios.post('https://localhost:7082/Leads/Add', lead,
             {
                 headers: headers
-            }).then(res => { })
-        return lead
+            }).then(res => {
+                return res.data
+            })
     })
 
 const LogInSlice = createSlice({
@@ -28,12 +29,12 @@ const LogInSlice = createSlice({
             state.thisUser = action.payload
             state.statusUser = "cust"
         },
-        UpdateUser:(state, action)=>{
+        UpdateUser: (state, action) => {
             state.thisUser = action.payload
         },
         logIn: (state, action) => {
             let user = action.payload.user
-            user.mail=user.mail.toLowerCase()
+            user.mail = user.mail.toLowerCase()
             const Customers = action.payload.Customers
             const Diagnosers = action.payload.Diagnosers
             const Leads = action.payload.Leads
@@ -41,28 +42,31 @@ const LogInSlice = createSlice({
             const lead = Leads.find(c => c.mail == user.mail && c.password == user.password)
             if (customer != null) {
                 state.statusUser = "cust"
-                state.thisUser=customer
+                state.thisUser = customer
             }
             else if (lead != null) {
                 state.statusUser = "cust"
-                state.thisUser=lead
+                state.thisUser = lead
             }
             else {
                 const diagnoser = Diagnosers.find(c => c.mail == user.mail && c.password == user.password)
                 if (diagnoser != null && diagnoser.mail == "22@2") {
                     state.statusUser = "Esty"
-                    state.thisUser=diagnoser
+                    state.thisUser = diagnoser
                 }
                 else if (diagnoser != null) {
                     state.statusUser = "diagnoser"
-                    state.thisUser=diagnoser
+                    state.thisUser = diagnoser
                 }
             }
             if (state.statusUser == null)
-                state.statusUser="wrong"
+                state.statusUser = "wrong"
         }
     },
     extraReducers: (builder) => {
+        builder.addCase(addLead.fulfilled, (state, action)=>{
+            state.thisUser.code = action.payload
+        })
     }
 })
 
